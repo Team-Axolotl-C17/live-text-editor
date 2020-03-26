a3199102a242a452ef2e4176e9045b495010c48e
mariodandrea <mariodandrea14@yahoo.com>
2020-03-25 20:09:45 -0400

    Added front end user activity

--- client/Containers/Editor-Container.js
+++ client/Containers/Editor-Container.js
@@ -15,11 +15,38 @@ class EditorContainer extends Component {
       consoleOutput: 'Console output will display here',
       room: 'Axolotl',
       username: auth.getUsername(),
+      users: [],
+      rooms: [],
     };
     // Listen for 'code sent from server'
     socket.on('code sent from server', (payload) => {
       this.updateCodeFromSockets(payload);
     });
+
+    socket.on('currentUsers', (payload) => {
+      let usersArr = [];
+      for (let i = 0; i < payload.length; i++) {
+        usersArr.push(<li>{payload[i]}</li>);
+      }
+      console.log('Payload');
+      console.log(payload);
+      this.setState({
+        users: usersArr,
+      });
+    });
+
+    socket.on('availableRooms', (payload) => {
+      // console.log('testing');
+      let roomsArr = [];
+      for (let i = 0; i < payload.length; i++) {
+        roomsArr.push(<option value={payload[i]}>{payload[i]}</option>);
+      }
+
+      this.setState({
+        rooms: roomsArr,
+      });
+    });
+
     this.updateCodeinState = this.updateCodeinState.bind(this);
     this.updateCodeFromSockets = this.updateCodeFromSockets.bind(this);
     this.runCode = this.runCode.bind(this);
@@ -69,6 +96,7 @@ class EditorContainer extends Component {
         <h1>Current Room: {this.state.room}</h1>
         <input ref="room" type="text" />
         <button onClick={this.clicked}>Create/Join Room</button>
+        <ul>{this.state.users}</ul>
         <Editor
           code={this.state.code}
           room={this.state.room}
